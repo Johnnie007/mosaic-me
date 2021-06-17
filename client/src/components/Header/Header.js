11df8a0d21e0189a115c4fa531f02814b0edcffc
Johnnie Hicks <johnnie@Johnnies-MBP.lan1>
2021-06-16 20:58:59 -0400

    added css to client-side

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,19 +1,20 @@
-
+import "./Header.scss";
 function Header(){
 
     /* todo
         Find font
-        Color Scheme
-        style
+        make responsive
     */
 
     return(
-        <header>
-            <div className="header__div">
-                <h1 className="header__h1">Mosaic Me</h1>
-            </div>
-            <div className="header__descrption">
-                <p>Mosaic Me is an app that will allow you to convert any image in a mosaic masterpiece. This app is simple to use! All you have to do is upload an image that you want to convert into a mosaic. Instead of using small pieces of colored stone, glass or ceramic to make your image. We will use other images as the pattern to make your masterpiece.</p>
+        <header className="header">
+                <div className="header__div">
+                    <div className="header__title">
+                        <h1 className="header__h1">Mosaic Me</h1>
+                    </div>
+                <div className="header__description">
+                    <p>Mosaic Me is an app that will allow you to convert any image in a mosaic masterpiece. This app is simple to use! All you have to do is upload an image that you want to convert into a mosaic. Instead of using small pieces of colored stone, glass or ceramic to make your image. We will use other images as the pattern to make your masterpiece.</p>
+                </div>
             </div>
         </header>
     )
--- client/src/components/SideBar/Sidebar.js
+++ client/src/components/SideBar/Sidebar.js
@@ -1,27 +1,27 @@
+import './Sidebar.scss';
 
 function Sidebar(){
     return(
-        <div>
-            <div className="main">
-                <div className="sidebar">
-                <form>
-                    <div className="sidebar__search">
-                        <input type="text" placeholder="Search(optional)"/>
-                        
-                    </div>
-                    <div className="sidebar__search">
-                        <input type="file" placeholder="Upload"/>
-                    </div>
-                    <div className="sidebar__button">
-                        <input type="radio"/>
-                        <label>Main</label>
-                        <input type="radio"/>
-                        <label>Pattern</label>
-                    </div>
-                </form>
+    <div className="sidebar">
+        <form className="sidebar__form">
+            <div className="sidebar__search">
+                <input className="sidebar__search--input" type="text" placeholder="Search(optional)"/>
+            </div>
+            <div className="sidebar__upload">
+                <input type="file" placeholder="Upload"/>
+            </div>
+            <div className="sidebar__button">
+                <div>
+                    <input className="sidebar__button--btn" type="radio" name="type"/>
+                    <label>Main</label>
+                </div>
+                <div>
+                    <input className="sidebar__button--btn" type="radio" name="type"/>
+                    <label>Pattern</label>
                 </div>
             </div>
-        </div>
+        </form>
+    </div>
     )
 }
 
