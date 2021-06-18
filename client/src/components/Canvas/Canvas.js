d5d8cbd7e126216f789dea219067c03619a2df97
Johnnie Hicks <johnnie@Johnnies-MBP.lan1>
2021-06-17 20:42:52 -0400

    added css and images to the client. Couldn't fiqure out how to use clientWidth for react

--- client/src/components/Canvas/Canvas.js
+++ client/src/components/Canvas/Canvas.js
@@ -1,7 +1,16 @@
-function Canvas (){
-    return(
-        <div className="">
+import "./Canvas.scss";
+import hey from  "../../Images/7.jpg"
+import React, {useRef} from 'react';
 
+
+function Canvas (){
+
+        return(
+            <div className="canvas-main" >
+                <div className="canvas-main__img"><img className="canvas-img" src={hey}/></div>   
         </div>
+        
     )
 }
+
+export default Canvas;
--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Sidebar from '../SideBar/Sidebar';
+import Canvas from '../Canvas/Canvas'
 
 class Main extends React.Component{
     render(){
         return(
             <div className="main">
                 <Sidebar/>
+                <Canvas />
             </div>
         )
     }
