88c0c5c1aae50e0027a3a56bca78badb540d08c1
Johnnie Hicks <johnnie@Johnnies-MacBook-Pro.local>
2021-06-18 14:05:16 -0400

    created mosaic feature

--- client/src/components/Canvas/Canvas.js
+++ client/src/components/Canvas/Canvas.js
@@ -1,16 +1,27 @@
 import "./Canvas.scss";
 import hey from  "../../Images/7.jpg"
-import React, {useRef} from 'react';
+import React from 'react';
+import axios from 'axios';
 
 
-function Canvas (){
+let mosaicDiv = []
+for(let i = 0; i < 400; i++ ){
+    mosaicDiv.push(<div className="test"><img src={hey}/></div>)
+    }
 
+class Canvas extends React.Component{
+    
+    render(){
         return(
-            <div className="canvas-main" >
-                <div className="canvas-main__img"><img className="canvas-img" src={hey}/></div>   
-        </div>
-        
-    )
+        <div className="canvas-main" >
+           <div className="canvas-main__img" style={{ backgroundImage: `url(${hey})`}}> 
+            {mosaicDiv.map((val) => {
+                return(val)
+            })}
+           </div> 
+        </div>  
+        )
+    }
 }
 
 export default Canvas;
