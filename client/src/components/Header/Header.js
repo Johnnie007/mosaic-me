fc27bf13ec8d8007e9e5492951ba3c010ce7b37e
Johnnie007 <81275727+Johnnie007@users.noreply.github.com>
2021-07-07 14:16:24 -0400

    updated instructions

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -15,6 +15,12 @@ function Header(){
                 <div className="header__div">
                     <div className="header__title">
                         <h1 className="header__h1" >Mosaic Me</h1>
+                        
+                    </div>
+                    <div className="header__text">
+                        <p>What is a Mosaic?</p>
+                        <p>Mosaic is a picture or pattern produced by arranging together small colored pieces of hard material, such as stone, tile, or glass.</p>
+                        <p>But instead of using hard material. This app will use images instead!</p>
                     </div>
                 <div className="header__link">
                    <Link to='/upload'><button className="header__link--a">Click here to begin</button></Link>
